b43c5eb714386f15299c9dce2067e0f5979098ca
VARUNKUMARSINGH1133 <144093365+VARUNKUMARSINGH1133@users.noreply.github.com>
2025-01-02 15:49:39 +0530

    Update AdminContext.jsx

--- admin/src/context/AdminContext.jsx
+++ admin/src/context/AdminContext.jsx
@@ -13,7 +13,7 @@ const AdminContextProvider=(props)=>{
     const[appointments,setAppointments]= useState([])
     const [doctors,setDoctors]= useState([])
     const [aToken,setAToken]= useState(localStorage.getItem('aToken')?localStorage.getItem('aToken'):'')
-    const backendUrl = import.meta.env.VITE_BACKEND_URL || 'http://localhost:4000';
+    const backendUrl = import.meta.env.VITE_BACKEND_URL || 'https://doctors-appointments-backend.onrender.com';
     const getAllDoctors= async()=>{
         try{
             const {data}= await axios.post(backendUrl+`/api/admin/all-doctors`,{},{headers:{aToken}})
